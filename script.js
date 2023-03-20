21b2704b38ea9f7cef4e9c504a9c94139409ccbe
Nton23 <huutonau@gmail.com>
2023-03-19 22:57:04 -0700

    edit the id typo of the div container

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 //create a div that contain other divs
 const divContainerDiv = document.createElement("div");
-divContainerDiv.setAttribute("id","div-cotnainer");
+divContainerDiv.setAttribute("id","div-container");
 
 //create a function to generate other divs
 function createDiv(divQty) {
     
 }
+
+//append the divs to html body
+document.body.appendChild(divContainerDiv);
+
+console.log("hello");
