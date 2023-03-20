74d7201e8535cb862caa3ad9d8f8a35150c94009
Nton23 <huutonau@gmail.com>
2023-03-19 22:47:08 -0700

    add div container

---
+++ script.js
@@ -0,0 +1,8 @@
+//create a div that contain other divs
+const divContainerDiv = document.createElement("div");
+divContainerDiv.setAttribute("id","div-cotnainer");
+
+//create a function to generate other divs
+function createDiv(divQty) {
+    
+}
